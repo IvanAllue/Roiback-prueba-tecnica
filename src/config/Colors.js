36d254c33784434f3c439a5e33859059ca44a078
Ivan Allue <ivanallue@outlook.com>
2022-02-13 20:15:50 +0100

    ROIBACK-PT-IVAN-13 - Components - Creado componente results en HotelSearchList.js y NoResultsComponent.js para cuando la query falla o no arroja resultados

--- src/config/Colors.js
+++ src/config/Colors.js
@@ -6,11 +6,15 @@ const PRIMARY_COLOR_DISABLED = '#b2dfde'
 // OTHER COLORS
 const BLACK = '#212121'
 const RED = '#f44336'
+const LIGHT_GREY = '#E1E1E1'
+const GREY = '#ACACAC'
 
 export const COLORS = {
     PRIMARY_COLOR,
     PRIMARY_COLOR_LIGHT,
     PRIMARY_COLOR_DISABLED,
     BLACK,
-    RED
+    RED,
+    LIGHT_GREY,
+    GREY
 }
--- src/containers/hotel-search/HotelSearchList.js
+++ src/containers/hotel-search/HotelSearchList.js
@@ -1,9 +1,26 @@
 import {FilterElements} from "../../componenets/FilterElements";
+import {SearchFailed} from "../../componenets/NoResultsComponent";
+import styled from "styled-components";
+
+const Results = styled.div`
+  padding-top: 5rem;
+  display: flex;
+  justify-content: center;
+  
+  @media (max-width: 950px) {
+    padding-top: 2rem;
+  }
+`
 
 export const HotelSearchList = () => {
+
     return (
         <div>
             <FilterElements></FilterElements>
+            <Results>
+                <SearchFailed title="CHECK AVAILABILITY"
+                              subtitle="Select a hotel and two dates and you will receive magical results"></SearchFailed>
+            </Results>
         </div>
 
     )
